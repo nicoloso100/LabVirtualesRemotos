a4a03daccd461fcee8466d298d75d23148d1225c
nicoloso100 <nico.las0315@hotmail.com>
2019-08-24 00:13:29 -0500

    Se insertó y configuró la plantilla de la aplicación

---
+++ aplicacion/src/pages/login/Login.js
@@ -0,0 +1,235 @@
+import React, { useState } from "react";
+import {
+  Grid,
+  CircularProgress,
+  Typography,
+  Button,
+  Tabs,
+  Tab,
+  TextField,
+  Fade,
+} from "@material-ui/core";
+import { withRouter } from "react-router-dom";
+import classnames from "classnames";
+
+// styles
+import useStyles from "./styles";
+
+// logo
+import logo from "./logo.svg";
+import google from "../../images/google.svg";
+
+// context
+import { useUserDispatch, loginUser } from "../../context/UserContext";
+
+function Login(props) {
+  var classes = useStyles();
+
+  // global
+  var userDispatch = useUserDispatch();
+
+  // local
+  var [isLoading, setIsLoading] = useState(false);
+  var [error, setError] = useState(null);
+  var [activeTabId, setActiveTabId] = useState(0);
+  var [nameValue, setNameValue] = useState("");
+  var [loginValue, setLoginValue] = useState("");
+  var [passwordValue, setPasswordValue] = useState("");
+
+  return (
+    <Grid container className={classes.container}>
+      <div className={classes.logotypeContainer}>
+        <img src={logo} alt="logo" className={classes.logotypeImage} />
+        <Typography className={classes.logotypeText}>
+          Laboratorios Virtuales Y Remotos
+        </Typography>
+      </div>
+      <div className={classes.formContainer}>
+        <div className={classes.form}>
+          <Tabs
+            value={activeTabId}
+            onChange={(e, id) => setActiveTabId(id)}
+            indicatorColor="primary"
+            textColor="primary"
+            centered
+          >
+            <Tab label="Login" classes={{ root: classes.tab }} />
+            <Tab label="New User" classes={{ root: classes.tab }} />
+          </Tabs>
+          {activeTabId === 0 && (
+            <React.Fragment>
+              <Typography variant="h1" className={classes.greeting}>
+                Bienvenido usuario!
+              </Typography>
+              <br />
+              <Typography className={classes.formDividerWord}>
+                Para ingresar escriba sus datos de acceso
+              </Typography>
+              <Fade in={error}>
+                <Typography color="secondary" className={classes.errorMessage}>
+                  Something is wrong with your login or password :(
+                </Typography>
+              </Fade>
+              <TextField
+                id="email"
+                InputProps={{
+                  classes: {
+                    underline: classes.textFieldUnderline,
+                    input: classes.textField,
+                  },
+                }}
+                value={loginValue}
+                onChange={e => setLoginValue(e.target.value)}
+                margin="normal"
+                placeholder="Usuario o correo electrónico"
+                type="email"
+                fullWidth
+              />
+              <TextField
+                id="password"
+                InputProps={{
+                  classes: {
+                    underline: classes.textFieldUnderline,
+                    input: classes.textField,
+                  },
+                }}
+                value={passwordValue}
+                onChange={e => setPasswordValue(e.target.value)}
+                margin="normal"
+                placeholder="Contraseña"
+                type="password"
+                fullWidth
+              />
+              <div className={classes.formButtons}>
+                {isLoading ? (
+                  <CircularProgress size={26} className={classes.loginLoader} />
+                ) : (
+                  <Button
+                    disabled={
+                      loginValue.length === 0 || passwordValue.length === 0
+                    }
+                    onClick={() =>
+                      loginUser(
+                        userDispatch,
+                        loginValue,
+                        passwordValue,
+                        props.history,
+                        setIsLoading,
+                        setError,
+                      )
+                    }
+                    variant="contained"
+                    color="primary"
+                    size="large"
+                  >
+                    Ingresar
+                  </Button>
+                )}
+                <Button
+                  color="primary"
+                  size="large"
+                  className={classes.forgetButton}
+                >
+                  Recuperar Contraseña
+                </Button>
+              </div>
+            </React.Fragment>
+          )}
+          {activeTabId === 1 && (
+            <React.Fragment>
+              <Typography variant="h1" className={classes.greeting}>
+                ¿Listo para comenzar?
+              </Typography>
+              <br />
+              <Typography variant="h2" className={classes.subGreeting}>
+                Ingresa los siguientes datos para crear tu cuenta
+              </Typography>
+              <Fade in={error}>
+                <Typography color="secondary" className={classes.errorMessage}>
+                  Something is wrong with your login or password :(
+                </Typography>
+              </Fade>
+              <TextField
+                id="name"
+                InputProps={{
+                  classes: {
+                    underline: classes.textFieldUnderline,
+                    input: classes.textField,
+                  },
+                }}
+                value={nameValue}
+                onChange={e => setNameValue(e.target.value)}
+                margin="normal"
+                placeholder="Nombre Completo"
+                type="email"
+                fullWidth
+              />
+              <TextField
+                id="email"
+                InputProps={{
+                  classes: {
+                    underline: classes.textFieldUnderline,
+                    input: classes.textField,
+                  },
+                }}
+                value={loginValue}
+                onChange={e => setLoginValue(e.target.value)}
+                margin="normal"
+                placeholder="Correo Electrónico"
+                type="email"
+                fullWidth
+              />
+              <TextField
+                id="password"
+                InputProps={{
+                  classes: {
+                    underline: classes.textFieldUnderline,
+                    input: classes.textField,
+                  },
+                }}
+                value={passwordValue}
+                onChange={e => setPasswordValue(e.target.value)}
+                margin="normal"
+                placeholder="Contraseña"
+                type="password"
+                fullWidth
+              />
+              <div className={classes.creatingButtonContainer}>
+                {isLoading ? (
+                  <CircularProgress size={26} />
+                ) : (
+                  <Button
+                    onClick={() =>
+                      loginUser(
+                        userDispatch,
+                        loginValue,
+                        passwordValue,
+                        props.history,
+                        setIsLoading,
+                        setError,
+                      )
+                    }
+                    disabled={
+                      loginValue.length === 0 ||
+                      passwordValue.length === 0 ||
+                      nameValue.length === 0
+                    }
+                    size="large"
+                    variant="contained"
+                    color="primary"
+                    fullWidth
+                    className={classes.createAccountButton}
+                  >
+                    Create your account
+                  </Button>
+                )}
+              </div>
+            </React.Fragment>
+          )}
+        </div>
+      </div>
+    </Grid>
+  );
+}
+
+export default withRouter(Login);
